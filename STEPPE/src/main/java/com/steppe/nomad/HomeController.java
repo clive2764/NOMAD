374a65f790d682128990672b5c2aeee8fde62a42
jha <jiwon.ha0513@gmail.com>
2017-08-10 11:10:55 +0900

    170810 11:10 지원 1차 커밋

--- STEPPE/src/main/java/com/steppe/nomad/HomeController.java
+++ STEPPE/src/main/java/com/steppe/nomad/HomeController.java
@@ -1,16 +1,22 @@
 package com.steppe.nomad;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.steppe.nomad.service.EmailManagement;
+import com.steppe.nomad.service.MemberManagement;
 @Controller
 public class HomeController {
 
 	private ModelAndView mav;
-	//페이지 처음 진입용
+	private EmailManagement em;
+
+	@Autowired
+	private MemberManagement mm;	//페이지 처음 진입용
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public ModelAndView home(Model model) {
 		mav = new ModelAndView();
@@ -48,6 +54,15 @@ public class HomeController {
 	public ModelAndView goAddProject(){
 		mav = new ModelAndView();
 		mav.setViewName("projectInsert"); //projectDetail.jsp 프로젝트 상세보기 페이지
+		return mav;
+	}
+
+	//이메일 인증
+	@RequestMapping(value = "/sendCode", method = RequestMethod.POST)
+	public ModelAndView sendCode() {
+		System.out.println("ModelAndView sendCode 시작");
+		mav = new ModelAndView(); 
+		mm.sendCode();
 		return mav;
 	}
 
