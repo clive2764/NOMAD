a51f70d316ec15edcdf172fc1401d85da8ee2d42
pc-PC\pc <during4277@gmail.com>
2017-08-10 12:58:19 +0900

    커밋

--- STEPPE/src/main/java/com/steppe/nomad/dao/FreelancerDao.java
+++ STEPPE/src/main/java/com/steppe/nomad/dao/FreelancerDao.java
@@ -1,26 +1,8 @@
 package com.steppe.nomad.dao;
 
-import java.util.List;
-
-import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
-import com.steppe.nomad.bean.Career;
 
 @Repository
 public class FreelancerDao {
 
-	@Autowired
-	private SqlSessionTemplate sqlSession;
-	
-	public int careerInsert(Career career) {
-		return sqlSession.insert("Career.careerInsert", career);
-		
-	}
-	
-	public int getCareerMaxNum() {
-		return sqlSession.selectOne("Career.getCareerMaxNum");
-	}
-	
 }
